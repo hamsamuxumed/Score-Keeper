112b648c8c66e63a9f25a21655c481692d1179df
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-01 11:43:01 +0100

    added event listerner for both player and set winning score to 5

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const p1Score = document.querySelector('#p1Score')
 const p2Score = document.querySelector('#p2Score')
 const p1btn = document.querySelector('#p1btn')
 const p2btn = document.querySelector('#p2btn')
-let count = 0;
+let p1Count = 0;
+let p2Count = 0;
+let winningScore = 5;
 
 p1btn.addEventListener('click', () => {
-    p1Score.textContent = count += 1
+    if (p1Count !== winningScore) {
+        p1Score.textContent = p1Count += 1
+    }
+})
+
+p2btn.addEventListener('click', () => {
+    if (p2Count !== winningScore) {
+        p2Score.textContent = p2Count += 1
+    }
 })
